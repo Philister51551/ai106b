9d6f9d514d0a46b9d9301325121597cc16f123fc
Philister <1026619787@qq.com>
2020-11-23 20:47:14 +0800

    hill climbing changed!

--- example/02-optimize/hillClimbingSimple.js
+++ example/02-optimize/hillClimbingSimple.js
@@ -1,4 +1,4 @@
-function f (x) { return x  + 1 / ( x * x )  }
+function f (x) { return -1 * (  x  + 1 / ( x * x ) )  }
 // function f(x) { return -1*Math.abs(x*x-4); }
 
 var dx = 0.2
@@ -6,17 +6,17 @@ var dx = 0.2
 function hillClimbing (f, x) {
   while (true) {
     //console.log('test=%s',(-1*(0.1+1/(0.1*0.1))).toFixed(4))
-    console.log('f(%s)=%s', x.toFixed(4), f(x).toFixed(4))
-    if(f(x + dx) <= f(x) && f(x - dx) <= f(x))
+    console.log('f(%s)=%s', x.toFixed(4), -1 * f(x).toFixed(4))
+    if(f(x + dx) >= f(x) && f(x - dx) >= f(x))
     {
-      if(f(x + dx) < f(x - dx)) {
+      if(f(x + dx) > f(x - dx)) {
         x = x + dx
       }else{
         x = x - dx
       }
-    }else if (f(x + dx) <= f(x)) {
+    }else if (f(x + dx) >= f(x)) {
       x = x + dx
-    } else if (f(x - dx) <= f(x)) {
+    } else if (f(x - dx) >= f(x)) {
       x = x - dx
     } else {
       break
